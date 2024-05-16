d6dbbd3e71098e1de108957740a13832be5af85c
higor335 <higor-lopo@hotmail.com>
2024-05-16 14:03:54 -0400

    feat: :sparkles: Função Listar Documentos

---
+++ listar.js
@@ -0,0 +1,36 @@
+function listarTodos(){
+
+}
+
+function listarDebito(){
+
+}
+
+function listarCredito(){
+
+}
+
+function listarPendentes(){
+
+}
+
+function listarAndamento(){
+
+}
+
+function listarConcluidos(){
+
+}
+
+function listarPorData(){
+
+}
+
+function Visualizar(){
+
+}
+
+function Excluir(){
+
+}
+
